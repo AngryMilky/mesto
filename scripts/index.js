c81fe7129fe652863aa975f4a806190d30e2f382
Milky <milky-maggie@mai.ru>
2023-02-16 18:52:46 +0300

    commit six sprint

--- scripts/index.js
+++ scripts/index.js
@@ -4,24 +4,28 @@ const buttonCloseElm = document.querySelector('.button-close-element');
 const buttonCloseImg = document.querySelector('.button-close-image');
 const buttonAdd = document.querySelector('.profile__button-add');
 
-
 const cards = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card-template').content;
+
+const popupPhoto = document.querySelector('.popup__photo');
+const popupCaption = document.querySelector('.popup__caption');
+
 
 const formEdit = document.querySelector('.popup_content_profile');
 const formAdd = document.querySelector('.popup_content_element');
 const formImg = document.querySelector('.popup_content_image');
 
-const formProfileEdit = document.getElementById('Profile_Edit');
-const formPlaceAdd = document.getElementById('Place_Add');
+const formProfileEdit = document.querySelector('#Profile_Edit');
+const formPlaceAdd = document.querySelector('#Place_Add');
 
 const name = document.querySelector('.profile__name');
 const job = document.querySelector('.profile__job');
 
-const nameText = document.getElementById('name')
-const jobText = document.getElementById('job')
+const nameText = document.querySelector('#name')
+const jobText = document.querySelector('#job')
 
-const nameImg = document.getElementById('img-name');
-const linkImg = document.getElementById('link');
+const nameImg = document.querySelector('#img-name');
+const linkImg = document.querySelector('#link');
 
 
 
@@ -29,11 +33,8 @@ const linkImg = document.getElementById('link');
 // создание карточки
 
 function createCard(nameCard, linkCard) {
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
 
-  const popupPhoto = document.querySelector('.popup__photo');
-  const popupCaption = document.querySelector('.popup__caption');
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
 
   cardElement.querySelector('.element__photo').src = linkCard;
   cardElement.querySelector('.element__captiontext').textContent = nameCard;
@@ -41,20 +42,17 @@ function createCard(nameCard, linkCard) {
 
 
   // поставить лайк
-
   cardElement.querySelector('.element__button-like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__button-like_active');
   });
 
   // удалить карточку
-
   cardElement.querySelector('.element__button-delete').addEventListener('click', function (evt) {
     const element = evt.target.closest('.element');
     element.remove();
   });
 
   // просмотреть фото
-
   cardElement.querySelector('.element__photo').addEventListener('click', function (evt) {
     openPopup(formImg);
     popupPhoto.src = evt.target.closest('.element__photo').src;
@@ -95,8 +93,7 @@ function handleFormSubmitAdd(evt) {
   const newCard = createCard(`${nameImg.value}`, `${linkImg.value}`);
   renderCard(newCard, cards);
 
-  linkImg.value = '';
-  nameImg.value = '';
+  evt.target.reset();
   closePopup(formAdd);
 }
 
@@ -105,14 +102,14 @@ function handleFormSubmitAdd(evt) {
 
 function closePopup(popupElement) {
   popupElement.classList.remove('popup_opened');
-  document.removeEventListener('keydown', keyHandler);
+  document.removeEventListener('keydown', closeByEsc);
 
 }
 
 
 // закрывает Попап нажатием Esc
 
-function keyHandler(evt) {
+function closeByEsc(evt) {
   if (evt.key == 'Escape') {
     closePopup(document.querySelector('.popup_opened'));
 
@@ -122,21 +119,21 @@ function keyHandler(evt) {
 
 // закрытие кликом по оверлей
 
-window.addEventListener('click', evt => {
-  const target = evt.target
-  if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__button-close')) {
+window.addEventListener('mousedown', evt => {
+  if (evt.target.classList.contains('popup_opened')) {
     closePopup(document.querySelector('.popup_opened'));
   }
-})
+});
 
 
 // открывает Попап 
 
 function openPopup(popupElement) {
   popupElement.classList.add('popup_opened');
-  document.addEventListener('keydown', keyHandler);
+  document.addEventListener('keydown', closeByEsc);
 
 }
+
 
 buttonEdit.addEventListener('click', function () {
   openPopup(formEdit);
@@ -146,8 +143,15 @@ buttonEdit.addEventListener('click', function () {
 });
 
 
+
 buttonAdd.addEventListener('click', function () {
   openPopup(formAdd);
+  
+const createButton = formAdd.querySelector('.popup__button-save');
+if ( nameImg.textContent.length === 0 && linkImg.textContent.length === 0 ) {
+  createButton.setAttribute('disabled', '');
+  createButton.classList.add('popup__button-save_disabled');
+}
 
 });
 
@@ -159,7 +163,7 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   job.textContent = `${jobText.value}`;
   name.textContent = `${nameText.value}`;
-  closePopup(popupElement);
+  closePopup(formEdit);
 
 }
 
