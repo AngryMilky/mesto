93b277b86265f2aa3cd51a39d39c14b1ea56aee7
Milky <milky-maggie@mai.ru>
2023-06-01 01:39:04 +0300

    sprint 9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,29 @@
-import Card from "../scripts/Card.js";
-import FormValidator from "../scripts/FormValidator.js";
-import Section from "../scripts/Section.js";
-import PopupWithImage from "../scripts/PopupWithImage.js";
-import PopupWithForm from "../scripts/PopupWithForm.js";
-import UserInfo from "../scripts/UserInfo.js";
-import PopupWithDelete from "../scripts/PopupWithDelete.js";
-import Api from "../scripts/Api.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+import PopupWithDelete from "../components/PopupWithDelete.js";
+import Api from "../components/Api.js";
 
 import '../pages/index.css';
 
-const buttonEdit = document.querySelector('.profile__button-edit');
-const buttonAdd = document.querySelector('.profile__button-add');
-const buttonAvatar = document.querySelector('.profile__avatar-edit-button');
+import {
+  buttonEdit, buttonAdd, buttonAvatar, cards, formDelete,
+  formAvatar, formImg, formEdit, formAdd, formProfileEdit,
+  formPlaceAdd, formAvatarEdit, nameText, jobText,
+  formValidationOptions
+} from '../utils/constants.js';
 
-const cards = document.querySelector('.elements');
+// Создаем валидацию форм
+const profileValidator = new FormValidator(formValidationOptions, formProfileEdit);
+const addCardValidator = new FormValidator(formValidationOptions, formPlaceAdd);
+const editAvatarValidator = new FormValidator(formValidationOptions, formAvatarEdit);
+profileValidator.enableValidation();
+addCardValidator.enableValidation();
+editAvatarValidator.enableValidation();
 
-const formDelete = document.querySelector('.popup_content_delete');
-const formAvatar = document.querySelector('.popup_edit_avatar');
-const formImg = document.querySelector('.popup_content_image');
-const formEdit = document.querySelector('.popup_content_profile');
-const formAdd = document.querySelector('.popup_content_element');
-
-const formProfileEdit = document.querySelector('#Profile_Edit');
-const formPlaceAdd = document.querySelector('#Place_Add');
-const formAvatarEdit = document.querySelector('#Edit_Avatar');
-
-const nameText = document.querySelector('#name');
-const jobText = document.querySelector('#job');
-
-const formValidationOptions = {
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button-save',
-  disabledButtonClass: 'popup__button-save_disabled',
-  inputErrorClass: 'popup__input_error',
-  errorClass: 'popup__error'
-};
 
 const api = new Api({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-66',
@@ -46,35 +35,17 @@ const api = new Api({
 
 let userId = null;
 
-
-api.getUserInfo()
-  .then((userData) => {
+Promise.all([api.getUserInfo(), api.getInitialCards()])
+  .then(([userData, initialCards]) => {
     profile.setUserInfo(userData);
     profile.setUserAvatar(userData);
     userId = userData._id;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
-
-api.getInitialCards()
-  .then((initialCards) => {
     cardsList.renderItems(initialCards);
-
   })
   .catch((err) => {
     console.log(err);
   });
-
-
-// Создаем валидацию форм
-const profileValidator = new FormValidator(formValidationOptions, formProfileEdit);
-const addCardValidator = new FormValidator(formValidationOptions, formPlaceAdd);
-const editAvatarValidator = new FormValidator(formValidationOptions, formAvatarEdit);
-profileValidator.enableValidation();
-addCardValidator.enableValidation();
-editAvatarValidator.enableValidation();
 
 
 function createCard(data, templateSelector) {
